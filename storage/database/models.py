8dee8dd89c473263c13d6b9c69757aca30638cdc
omi <sowmithbasak13@gmail.com>
2026-06-03 09:29:05 +0530

    fix: missed a few files during merge

--- storage/database/models.py
+++ storage/database/models.py
@@ -2,11 +2,7 @@ import enum
 from datetime import datetime
 from uuid import uuid4
 
-<<<<<<< HEAD
-from sqlalchemy import String, Text, JSON, DateTime, Integer, Float # content-types
-=======
 from sqlalchemy import String, Text, JSON, DateTime, Integer # content-types
->>>>>>> upstream/main
 from sqlalchemy import Enum, ForeignKey # for schema
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -74,26 +70,6 @@ class ExtractedContent(Base):
     extraction_metadata: Mapped[dict] = mapped_column(JSON, nullable = True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
-<<<<<<< HEAD
-# Worker Result table for better data handling
-
-class WorkerResult(Base):
-    __tablename__ = "worker_results"
-
-    id: Mapped[str] = mapped_column(String, primary_key=True, default=lambda: str(uuid4()))
-    asset_id: Mapped[str] = mapped_column(String, ForeignKey("assets.id"), nullable=False)
-
-    # type of worker ["pdf", "image", "audio"] and associated data
-    worker_type: Mapped[str] = mapped_column(String, nullable=False)
-    data: Mapped[dict] = mapped_column(JSON, nullable=False)
-
-    # confidence + processing status + time of creation
-    confidence: Mapped[float] = mapped_column(Float, nullable=True)
-    status: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
-
-=======
->>>>>>> upstream/main
 # CHUNKING TIME
 
 class ContentChunk(Base):
@@ -102,11 +78,7 @@ class ContentChunk(Base):
     id: Mapped[str] = mapped_column(String,primary_key=True,default=lambda: str(uuid4()))
     asset_id: Mapped[str] = mapped_column(String, ForeignKey("assets.id"), nullable=False)
 
-<<<<<<< HEAD
-    worker_result_id: Mapped[str] = mapped_column(String, ForeignKey("worker_results.id"),nullable=False)
-=======
     extracted_content_id: Mapped[str] = mapped_column(String, ForeignKey("extracted_content.id"),nullable=False)
->>>>>>> upstream/main
 
     chunk_idx: Mapped[int] = mapped_column(Integer, nullable=False)
     chunk_type: Mapped[str] = mapped_column(String,nullable=False)
@@ -115,9 +87,6 @@ class ContentChunk(Base):
 
     created_at: Mapped[datetime] = mapped_column(DateTime,default=datetime.now)
 
-<<<<<<< HEAD
-
-=======
 class AssetOutput(Base):
     __tablename__ = "asset_outputs"
 
@@ -144,4 +113,3 @@ class AssetOutput(Base):
     created_at: Mapped[datetime] = mapped_column(
         DateTime, default=datetime.now
     )
->>>>>>> upstream/main
